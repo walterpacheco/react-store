66a84370a9a5f6404d105a149a1081a41a53ad13
Walter Pacheco <walter.pachecoch@gmail.com>
2026-06-11 20:52:02 -0400

    feat: crear componente header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,17 +1,42 @@
 import "./Header.css";
-import { headerData } from "./header.data";
+import { headerData } from "./Header.data";
 
 function Header() {
   return (
-    <header className="header">
-      <h1>{headerData.logo}</h1>
-      <nav>
-        {headerData.links.map((link) => (
-          <a key={link.id} href={link.href}>
-            {link.label}
-          </a>
-        ))}
-      </nav>
+    <header className="header navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="#">
+          {headerData.logo}
+        </a>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+           
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <div className="navbar-nav ms-auto">
+                {headerData.links.map((link) => (
+                  <a
+                    key={link.id}
+                    className="nav-link nav-spacing"
+                    href={link.href}
+                  >
+                    {link.label}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
